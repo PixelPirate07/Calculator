6d6e724a269cd39f644e23480e6db0ba80b9d2a0
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-17 13:43:27 +0530

    committed

--- Calc.java
+++ Calc.java
@@ -6,7 +6,7 @@ public class Calc {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.println((a + b) + (a - b));
+        System.out.println((a + b) + (a - b) + (a * b));
         sc.close();
     }
 
