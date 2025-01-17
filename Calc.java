bcb1e75da5f863950a82dba2ce25317c2d6cf64c
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-17 15:35:30 +0530

    committed

--- Calc.java
+++ Calc.java
@@ -5,10 +5,19 @@ public class Calc {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
+        int c = sc.nextInt();
         System.out.println(
                 "addition" + (a + b) + "subtraction" + (a - b) + "multiplication" + (a * b) + "division" + (a / b)
                         + "smallest" + (Math.min(a, b)));
+
+        // comparison of three numbers
+        if (a > b && a > c) {
+            System.out.println("a is greater");
+        } else if (b > a && b > c) {
+            System.out.println("b is greater");
+        } else {
+            System.out.println("c is greater");
+        }
         sc.close();
     }
 
