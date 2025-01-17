f669d0ef557f79014327bdb19a71d3a3b887ba29
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-17 13:56:41 +0530

    committed

--- Calc.java
+++ Calc.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class Calc {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int z = x / y;
-        System.out.println(z);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a / b);
         sc.close();
     }
 }
