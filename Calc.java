b9c2fd04c8a2a82cfcce7748c97e1ff68a33314c
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-17 12:11:11 +0530

    committed

--- Calc.java
+++ Calc.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String args[]) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a - b;
+        System.out.println(c);
+        sc.close();
     }
 }
